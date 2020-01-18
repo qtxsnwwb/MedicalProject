bbd4a1cf3a45b6c535232c63b32cd3ae5e8dad99
wwb <1624949784@qq.com>
2020-01-18 23:07:40 +0800

    设置门诊查询到医生查询的跳转

--- app/src/main/java/com/example/lenovo/medicalProject/DoctorActivity.java
+++ app/src/main/java/com/example/lenovo/medicalProject/DoctorActivity.java
@@ -2,6 +2,7 @@ package com.example.lenovo.medicalProject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.lenovo.myapplication.R;
 
@@ -11,9 +12,15 @@ import com.example.lenovo.myapplication.R;
  */
 public class DoctorActivity extends AppCompatActivity {
 
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        tv = (TextView)findViewById(R.id.demo);
+        String officeName = getIntent().getStringExtra("officeName");
+        tv.setText(officeName);
     }
 }
