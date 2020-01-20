87b580a9d810071599da524a9abdca23218dbd77
wwb <1624949784@qq.com>
2020-01-20 22:39:16 +0800

    增加移动端登录注册页

--- app/src/main/java/com/example/lenovo/medicalProject/MainActivity.java
+++ app/src/main/java/com/example/lenovo/medicalProject/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, DiagnoseActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
 
         System.out.println();
