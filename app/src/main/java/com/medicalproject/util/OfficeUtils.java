d3d6752afdcce126e16dc69302b82cf4eab95140
wwb <1624949784@qq.com>
2020-01-22 20:11:11 +0800

    修改开药方主页，申请科室值根据编号获取，新增OfficeUtils类

---
+++ app/src/main/java/com/medicalproject/util/OfficeUtils.java
@@ -0,0 +1,46 @@
+package com.medicalproject.util;
+
+import java.util.HashMap;
+
+/**
+ * 科室编号映射工具类
+ * @author 汪文博
+ */
+public class OfficeUtils {
+
+    public static HashMap<String, String> map = new HashMap<>();
+    static {
+        map.put("1", "心脏与血管外科");
+        map.put("2", "肿瘤科");
+        map.put("3", "男科");
+        map.put("4", "整形美容科");
+        map.put("5", "神经外科");
+        map.put("6", "内分泌与代谢科");
+        map.put("7", "皮肤性病科");
+        map.put("8", "呼吸内科");
+        map.put("9", "内科");
+        map.put("10", "眼科");
+        map.put("11", "普外科");
+        map.put("12", "烧伤科");
+        map.put("13", "口腔颌面科");
+        map.put("14", "肾内科");
+        map.put("15", "甲状腺乳腺外科");
+        map.put("16", "精神科");
+        map.put("17", "风湿免疫科");
+        map.put("18", "泌尿外科");
+        map.put("19", "耳鼻喉科");
+        map.put("20", "神经内科");
+        map.put("21", "消化内科");
+        map.put("22", "妇科");
+        map.put("23", "心理科");
+        map.put("24", "感染科");
+        map.put("25", "骨科");
+        map.put("26", "血液病科");
+        map.put("27", "心血管内科");
+        map.put("28", "胸外科");
+        map.put("29", "儿科");
+        map.put("30", "产科");
+        map.put("31", "新生儿科");
+        map.put("32", "急诊科");
+    }
+}
